948a88c7fe8428c7ebc49709a22de4263b3baa51
Mert Demir <>
2021-06-01 16:32:34 +0000

    i lost track of changes :D that is all for audio until i find a working solution for queue and stuff

---
+++ cogs/xp.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+
+class Xp(commands.cog):
+  def __init__(self, bot):
+    self.bot = bot
+
+
+
+#gain xp by being active
+#messages
+#voice
+#and clever failsafe needed
